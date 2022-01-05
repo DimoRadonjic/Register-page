9a234ab8a0fd65a224f540edc4ce60110d888ea5
DimoRadonjic <dimo.radonjic2112@gmail.com>
2022-01-05 16:14:35 +0100

    Template done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return <div className='App'>Works</div>;
+class App extends Component {
+  state = {
+    username: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  render() {
+    return (
+      <section className='login-register-content'>
+        <div className='card-content'>
+          <div className='card-header'>
+            <h2>Login & Register</h2>
+            <p>Login & Register Description</p>
+          </div>
+
+          <div className='card-body'>
+            <form autoComplete='off' method='post'>
+              <input
+                type='text'
+                autoComplete='off'
+                name='username'
+                placeholder='Username'
+              />
+              <input
+                required
+                type='text'
+                name='firstName'
+                placeholder='Enter you First Name'
+                autoComplete='off'
+              />
+              <input
+                required
+                type='text'
+                name='lastName'
+                placeholder='Enter you Last Name'
+                autoComplete='off'
+              />
+              <input
+                required
+                type='email'
+                name='email'
+                placeholder='Enter you email'
+                autoComplete='off'
+              />
+              <input
+                required
+                type='password'
+                name='password'
+                placeholder='Password'
+                autoComplete='off'
+              />
+              <button type='submit'>Login & Register</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
